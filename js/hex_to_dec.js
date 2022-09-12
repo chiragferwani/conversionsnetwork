3c499ce9446f6f58f455e529d38aab4d71284d9f
Chirag Ferwani <chiragferwani@gmail.com>
2022-09-12 22:55:34 +0530

    commit

--- js/hex_to_dec.js
+++ js/hex_to_dec.js
@@ -15,14 +15,45 @@ binInp.addEventListener("input", () => {
   let binValue = binInp.value;
   //If the hex number is valid convert it to decimal
   if (binValidator(binValue)) {
-    decInp.value = parseInt(binValue, 16);
-    console.log(decInp.value);
+    decInp.value = returnNum;
     errorMsg.textContent = "";
   }
-  //Else display an error message
+  var hexToDec = function(src) {
+    
+    var mapping = {
+        "0" : "0",
+        "1" : "1",
+        "2" : "2",
+        "3" : "3",
+        "4" : "4",
+        "5" : "5",
+        "6" : "6",
+        "7" : "7",
+        "8" : "8",
+        "9" : "9",
+        "A" : "10",
+        "B" : "11",
+        "C" : "12",
+        "D" : "13",
+        "E" : "14",
+        "F" : "15"
+    };
+    
+    var srcString = src.toString();
+    var returnNum = 0;
+    var i;
+    
+    for (i = 0; i < srcString.length; i++) {
+        returnNum += mapping[srcString[i]] * (16 ** (srcString.length -1 - i));
+    }
+    
+    return returnNum;
+    
+};
+  /*Else display an error message
   else {
     errorMsg.textContent = "Please Enter An Valid Hex Input";
-  }
+  }*/
 
   //Function to check if the hex number is valid 
   function binValidator(num) {
